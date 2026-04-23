f08d7abafc730513b021eb7d0ddc893ef2b3fc9d
kubotera <kubotera@C010423232.zipangu.cac.co.jp>
2026-04-23 14:11:56 +0900

    lesson2_7完了

--- Java100_questions_cac2nd/src/lesson02/challenge07/Detective.java
+++ Java100_questions_cac2nd/src/lesson02/challenge07/Detective.java
@@ -25,7 +25,9 @@
 
 package lesson02.challenge07;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Detective {
 
@@ -39,6 +41,17 @@ public class Detective {
 		//ここに入力処理を入れる
 
 		//ここにif～else文を入れる
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String str = reader.readLine();
+		int height = Integer.parseInt(str);
+		//ここにif～else文を入れる
+		if (height > 185) {
+			System.out.println("探偵：");
+			System.out.println("その身長だと犯行が可能ですね。");
+		} else {
+			System.out.println("探偵：");
+			System.out.println("その身長だと犯行は無理ですね。");
+		}
 
 	}
 
