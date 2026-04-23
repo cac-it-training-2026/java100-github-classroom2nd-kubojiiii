64abab086a5d49224ea9a1d977f2212ae9df5ae6
kubotera <kubotera@C010423232.zipangu.cac.co.jp>
2026-04-23 13:51:53 +0900

    lesson1_10完了

---
+++ Java100_questions_cac2nd/src/lesson01/challenge10/Patisserie.java
@@ -0,0 +1,14 @@
+package lesson01.challenge10;
+
+public class Patisserie {
+
+	public static void main(String[] args) {
+
+		System.out.println("居酒屋バイト募集！！");
+		System.out.println("勤務時間12:00～24:00");
+		System.out.println("時給1200円");
+		System.out.println("初心者大歓迎！まかない食べ放題！！");
+
+	}
+
+}
