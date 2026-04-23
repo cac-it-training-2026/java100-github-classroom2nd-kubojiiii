e1c4de650da4259135ae9a959777e0db64a3187d
kubotera <kubotera@C010423232.zipangu.cac.co.jp>
2026-04-23 16:27:23 +0900

    lesson4_6完了

--- Java100_questions_cac2nd/src/lesson04/challenge06/WarehouseManager.java
+++ Java100_questions_cac2nd/src/lesson04/challenge06/WarehouseManager.java
@@ -38,10 +38,26 @@ public class WarehouseManager {
 	public static void main(String[] args) {
 
 		int[] ABKosanArray = new int[5];
-
-
+		int intputNum = 0;
+		boolean loopFlag = false;
 		//ここに重複チェックおよび値の代入処理を記述する
+		for (int i = 0; i < ABKosanArray.length; i++) {
 
+			do {
+				loopFlag = false;
+				intputNum = (int) (Math.random() * 10) % 5 + 1;
+
+				for (int j = 0; j < ABKosanArray.length; j++) {
+					if (ABKosanArray[j] == intputNum) {
+						loopFlag = true;
+						break;
+					}
+				}
+
+			} while (loopFlag);
+
+			ABKosanArray[i] = intputNum;
+		}
 
 		System.out.println("E主任：");
 		System.out.println("AB興産の荷物の入れ替えをお願いします。\n");
@@ -57,9 +73,7 @@ public class WarehouseManager {
 		}
 		System.out.println("\nです。\n");
 
-
 		//ここに値の入れ替え処理を記述する
-
 
 		System.out.println("入れ替え後の状態は、");
 		for (int i = 0; i < ABKosanArray.length; i++) {
